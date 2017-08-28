d2672f3fe1bcca4672eb8dc5fde204ef6674749b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-28 19:09:00 +0200

    Replace input names, export-as, attribute selectors in templates

--- src/material/component-data.ts
+++ src/material/component-data.ts
@@ -11,14 +11,19 @@ export interface MaterialAttributeSelector {
 }
 
 /** Export the class name data as part of a module. This means that the data is cached. */
-export const classNames: MaterialNameData[] =
-  require('./data/class-names.json');
+export const classNames: MaterialNameData[] = require('./data/class-names.json');
 
-/** Export the class name data as part of a module. This means that the data is cached. */
-export const elementSelectors: MaterialNameData[] =
-  require('./data/element-selectors.json');
+/** Export the input names data as part of a module. This means that the data is cached. */
+export const inputNames: MaterialNameData[] = require('./data/input-names.json');
 
-/** Export the class name data as part of a module. This means that the data is cached. */
+/** Export the element selectors data as part of a module. This means that the data is cached. */
+export const elementSelectors: MaterialNameData[] = require('./data/element-selectors.json');
+
+
+/** Export the attribute selectors data as part of a module. This means that the data is cached. */
+export const exportAsNames: MaterialNameData[] = require('./data/export-as-names.json');
+
+
+/** Export the attribute selectors data as part of a module. This means that the data is cached. */
 export const attributeSelectors: MaterialAttributeSelector[] =
   require('./data/attribute-selectors.json');
-
--- src/rules/switchTemplatesRule.ts
+++ src/rules/switchTemplatesRule.ts
@@ -1,7 +1,10 @@
 import {Rules, RuleFailure} from 'tslint';
-import {TemplateWalker} from '../angular/template-walker';
-import {ExternalTemplate} from '../angular/template-file';
-import {elementSelectors} from '../material/component-data';
+import {TemplateWalker} from '../tslint/template-walker';
+import {ExternalTemplate} from '../tslint/template-file';
+import {
+  attributeSelectors, elementSelectors, exportAsNames,
+  inputNames
+} from '../material/component-data';
 import * as ts from 'typescript';
 
 /**
@@ -9,6 +12,10 @@ import * as ts from 'typescript';
  * outdated prefix.
  */
 const failureMessage = 'Template uses outdated Material prefix.';
+
+/** Method that can be used to remove the surrounding Angular attribute brackets from a string. */
+const removeAttributeBackets = (str) => str.substring(1, str.length - 1);
+
 /**
  * Rule that walks through every string literal, which includes the outdated Material prefix and
  * is part of a call expression. Those string literals will be changed to the new prefix.
@@ -56,7 +63,23 @@ export class SwitchTemplatesWalker extends TemplateWalker {
       templateContent = templateContent.replace(`</${selector.md}`, `</${selector.mat}`);
     });
 
+    attributeSelectors.forEach(attribute => {
+      templateContent = templateContent.replace(
+        removeAttributeBackets(attribute.md),
+        removeAttributeBackets(attribute.mat)
+      );
+    });
+
+    inputNames.forEach(input => {
+      templateContent = templateContent.replace(input.md, input.mat);
+    });
+
+    exportAsNames.forEach(exportName => {
+      templateContent = templateContent.replace(exportName.md, exportName.mat);
+    });
+
     return templateContent;
   }
+
 
 }
