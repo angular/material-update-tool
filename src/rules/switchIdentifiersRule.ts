8e97c332e2bceef7679eb6eff515a50d7d32ccb5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-09-25 22:41:07 +0200

    fix: safety check for resolved symbol
    * Checks if the symbol for the identifier could be resolved and includes information about the identifier.

--- src/rules/switchIdentifiersRule.ts
+++ src/rules/switchIdentifiersRule.ts
@@ -37,12 +37,8 @@ export class SwitchIdentifiersWalker extends ProgramAwareRuleWalker {
 
   /** Method that is called for every identifier inside of the specified project. */
   visitIdentifier(identifier: ts.Identifier) {
-    // In some situations, developers will import Angular Material completely using a namespace
-    // import. This is not recommended, but should be still handled in the migration tool.
-    if (isNamespaceImportNode(identifier) && isMaterialImportDeclaration(identifier)) {
-      const symbol = getOriginalSymbolFromNode(identifier, this.getTypeChecker());
-      return this.materialNamespaceDeclarations.push(symbol.valueDeclaration);
-    }
+    // Store Angular Material namespace identifers in a list of declarations.
+    this._storeNamespaceImports(identifier);
 
     // For identifiers that aren't listed in the className data, the whole check can be
     // skipped safely.
@@ -50,29 +46,35 @@ export class SwitchIdentifiersWalker extends ProgramAwareRuleWalker {
       return;
     }
 
-    const originalSymbol = getOriginalSymbolFromNode(identifier, this.getTypeChecker());
+    const symbol = getOriginalSymbolFromNode(identifier, this.getTypeChecker());
+
+    // If the symbol is not defined or could not be resolved, just skip the following identifier
+    // checks.
+    if (!symbol) {
+      return;
+    }
 
     // For export declarations that are referring to Angular Material, the identifier should be
     // switched to the new prefix.
     if (isExportSpecifierNode(identifier) && isMaterialExportDeclaration(identifier)) {
-      return this.createIdentifierFailure(identifier, originalSymbol);
+      return this.createIdentifierFailure(identifier, symbol);
     }
 
     // For import declarations that are referring to Angular Material, the value declarations
     // should be stored so that other identifiers in the file can be compared.
     if (isImportSpecifierNode(identifier) && isMaterialImportDeclaration(identifier)) {
-      this.materialDeclarations.push(originalSymbol.valueDeclaration);
+      this.materialDeclarations.push(symbol.valueDeclaration);
     }
 
     // For identifiers that are not part of an import or export, the list of Material declarations
     // should be checked to ensure that only identifiers of Angular Material are updated.
     // Identifiers that are imported through an Angular Material namespace will be updated.
-    else if (this.materialDeclarations.indexOf(originalSymbol.valueDeclaration) === -1 &&
+    else if (this.materialDeclarations.indexOf(symbol.valueDeclaration) === -1 &&
               !this._isIdentifierFromNamespace(identifier)) {
       return;
     }
 
-    return this.createIdentifierFailure(identifier, originalSymbol);
+    return this.createIdentifierFailure(identifier, symbol);
   }
 
   /** Creates a failure and replacement for the specified identifier. */
@@ -88,6 +90,19 @@ export class SwitchIdentifiersWalker extends ProgramAwareRuleWalker {
         identifier.getStart(), identifier.getWidth(), classData.mat);
 
     this.addFailureAtNode(identifier, failureMessage, replacement);
+  }
+
+  /** Checks namespace imports from Angular Material and stores them in a list. */
+  private _storeNamespaceImports(identifier: ts.Identifier) {
+    // In some situations, developers will import Angular Material completely using a namespace
+    // import. This is not recommended, but should be still handled in the migration tool.
+    if (isNamespaceImportNode(identifier) && isMaterialImportDeclaration(identifier)) {
+      const symbol = getOriginalSymbolFromNode(identifier, this.getTypeChecker());
+
+      if (symbol) {
+        return this.materialNamespaceDeclarations.push(symbol.valueDeclaration);
+      }
+    }
   }
 
   /** Checks whether the given identifier is part of the Material namespace. */
