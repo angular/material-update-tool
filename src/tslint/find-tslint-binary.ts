8503735b4c8fc9717c4a53de23732fc6dd776264
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-10-10 09:38:20 +0200

    fix(): replace outdated stepper imports
    * Fixes that stepper selectors, class names are not being updated
    * Better error messages if symbol could not be resolved.
    * Instructions after successful migration
    * Note about required Angular Material version on migration failure.
    * Better TSLint binary resolving without runtime exception from `resolveBin`.
    * Safety check in `ComponentWalker` for decorators without metadata.

--- src/tslint/find-tslint-binary.ts
+++ src/tslint/find-tslint-binary.ts
@@ -2,16 +2,15 @@ import {resolve} from 'path';
 import {existsSync} from 'fs';
 
 // This import lacks of type definitions.
-const resolveBin = require('resolve-bin').sync;
+const resolveBinSync = require('resolve-bin').sync;
 
 /** Finds the path to the TSLint CLI binary. */
 export function findTslintBinaryPath() {
   const defaultPath = resolve(__dirname, '..', 'node_modules', 'tslint', 'bin', 'tslint');
-  const resolvedPath = resolveBin('tslint', 'tslint');
 
   if (existsSync(defaultPath)) {
     return defaultPath;
   } else {
-    return resolvedPath;
+    return resolveBinSync('tslint', 'tslint');
   }
 }
